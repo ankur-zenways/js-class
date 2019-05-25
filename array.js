0c586d660f53f372684b74acd7ddf2a396766971
Gaurav <gaurav.sharma@zenways.io>
2019-05-25 13:16:14 +0530

    function added after array

---
+++ array.js
@@ -0,0 +1,91 @@
+// const abc = [];
+// abc = 1233;
+
+// let abc = 'abc';
+// abc = 123
+
+// condition = 'hello';
+
+// if(condition == 'hello'){
+//     const abc = 'hello world!'
+// }
+
+// console.log(abc);
+
+// const abc = 'hello world';
+// abc = 'something else';
+
+// const tempObj = {}
+// tempObj['newProp'] = 'hello world';
+// delete tempObj.newProp;
+
+const tempArray = ["a", "b", "c", "d", "e", "f"];
+
+// tempArray.push('abc');
+
+// console.log(tempArray[0], tempArray.length, tempArray[tempArray.length - 1]);
+
+// for(let i = 0; i < tempArray.length; i++){
+//     console.log(tempArray[i])
+// }
+
+// console.log(i)
+
+// tempArray.forEach( function )
+
+// Functions
+
+// myFn(20, 10, 30, 40);
+
+// function myFn(myParam1, myParam2, myParam3, myParam4){
+//     // console.log(myParam)
+
+//     const newString = 'My lucky number is ' + myParam1
+
+//     console.log(newString);
+// }
+
+// myNewFn('20')
+
+// const myNewFn = function(myParam1){
+//     const newString = 'My lucky number is ' + myParam1;
+//     console.log(newString);
+// }
+
+// IIFE
+// (function(myParam1){
+//     const newString = 'My lucky number is ' + myParam1;
+//     console.log(newString);
+// })('20');
+
+// const myArrFn = (myParam1, myParam2) => {
+// //   const newString = "My lucky number is " + myParam1 + "and" + myParam2;
+
+// const newString = `My lucky number is ${myParam1} and ${myParam2}`;
+//   console.log(newString);
+// };
+
+// myArrFn("20", "30");
+
+// const myArrFn = myParam1 => {
+//   //   const newString = "My lucky number is " + myParam1 + "and" + myParam2;
+
+//   const newString = `My lucky number is ${myParam1}`;
+//   console.log(newString);
+// };
+
+// myArrFn("20");
+
+// const myArrFn = myParam1 => console.log(`My lucky number is ${myParam1}`);
+
+// myArrFn("20");
+
+const square = num => num * num;
+
+// function square(num){
+//     return num * num
+// }
+
+fourSquare = square(4);
+
+console.log(fourSquare);
