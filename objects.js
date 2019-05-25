712d3a01dce2419cf3b7ada4b6c6c48ce560eef8
Gaurav <gaurav.sharma@zenways.io>
2019-05-25 12:05:08 +0530

    Objects and Strings

---
+++ objects.js
@@ -0,0 +1,72 @@
+let myObj = {
+    name: 'Ankur',
+    class: '10th',
+    roll_no: 173698,
+    age: 24
+}
+
+// delete myObj["name"];
+// delete myObj["age"];
+
+// console.log(Object.keys(myObj))
+// console.log(Object.values(myObj))
+
+// console.log(typeof myObj)
+
+// console.log(myObj.name, myObj["age"])
+
+
+// myObj.grades = 90;
+// myObj["grades"] = 90;
+
+// myObj.name = "bob"
+// myObj["name"] = 'bob'
+
+//deleting any keys from an object.
+//using dot notation
+// delete myObj.roll_no
+
+//using square notation
+// delete myObj["roll_no"]
+
+// console.log(myObj)
+
+// objKeys - camel notation
+//  ObjKeys - pascal notation
+
+//accessing keys of an object
+// let objKeys = Object.keys(myObj)
+
+//accessing values of an object
+// let objValues = Object.values(myObj)
+
+// console.log(typeof myObj)
+// console.log(myObj)
+
+//there are two methods to access an object.
+
+// dot notation
+// console.log('This is my roll number' + myObj.roll_no)
+
+// square brakcet nottation
+// console.log('This is my name' + ' ' + myObj["name"])
+
+// backtick notation
+// console.log(`This is my name ${myObj["name"]}`)
+
+//adding keys and values to objects
+//dot nottation
+// myObj.percent = "100%"
+
+//bracket notation
+// myObj['percent'] = "50%"
+
+//changing values in a object
+//dot notation
+// myObj.name = 'Jhon'
+
+//square notation
+// myObj["name"] = 'Abc'
+
+// console.log(myObj)
+
---
+++ string.js
@@ -0,0 +1,78 @@
+// string in js
+
+// single quotes
+// let firstName = '    ankur';
+
+// double quotes
+// let lastName = "atri"
+
+//backticks
+// let c = `i am ankur`
+
+//my intials
+
+// let myIntials = firstName.charAt(0).concat(lastName.charAt(0))
+
+// console.log(myIntials.toUpperCase())
+
+// console.log(firstName)
+
+
+
+//charAt
+// let charc = a.charAt(0)
+
+// console.log(charc.toUpperCase())
+
+//string concatination
+
+// concatination with backticks
+// let con = `${a} ${b} ${c}`
+// console.log(con)
+
+// concating with library functions
+// let con = a.concat(b).concat(c)
+// console.log(con)
+
+// let a = 1;
+
+// let b = a.toString()
+
+// console.log(typeof b)
+
+// let a = "a quick browned fox jumps over a little lazy dog"
+
+// let split = a.split(" ")
+
+// let reverseArr = split.reverse()
+
+// // console.log(reverseArr)
+
+// console.log(reverseArr.join(" "))
+
+// let a = "My name is Ankur"
+
+// console.log(a.replace("Ankur", "Sachin"))
+
+
+// let str = "hello, how are you?"
+
+// console.log(str.includes("how"))\
+
+//falsy values
+
+// false
+// null
+// undefined
+// 0
+// ''
+// NaN
+
+
+
+
+
+
+
+
+
